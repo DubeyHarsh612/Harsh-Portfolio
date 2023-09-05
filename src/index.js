186a742fb4e1c982fa66cbc59a31f184f14d843b
harsh612222 <110035803+harsh612222@users.noreply.github.com>
2023-09-05 13:21:35 +0530

    react dom in index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom'; // Import ReactDOM from 'react-dom'
 
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,6 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
-
